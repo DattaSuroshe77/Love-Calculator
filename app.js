4e1ad45290abfb12f35c9c909ce4f701e11f4f94
Datta suroshe <141153932+DattaSuroshe77@users.noreply.github.com>
2023-09-17 12:35:56 +0530

    JavaScript file

---
+++ app.js
@@ -0,0 +1,28 @@
+function calculateLove(){
+    const name1 =document.getElementById("name1").value.trim();
+    const name2= document.getElementById("name2").value.trim();
+
+if(name1 === "" || name2 === ""){
+    alert("Please enter both names")
+}
+
+const loverPercentage= Math.floor(Math.random() * 101);
+
+const result = document.getElementById("result");
+
+
+
+result.innerHTML =` ${name1} and ${name2} 's Love Percentage : <span>${loverPercentage}%</span>`
+
+if(loverPercentage < 30){
+    result.innerHTML += "<br> Not a Great Macth. Keeping Looking !"
+
+}else if(loverPercentage> 30 && loverPercentage<70){
+    result.innerHTML += "<br> There is potential. Give it a try !"
+
+}
+else{
+    result.innerHTML += "<br> Great Match ! Love is in The air!"
+}
+
+}
